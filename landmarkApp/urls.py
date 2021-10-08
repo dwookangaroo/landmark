73fc19b8d5b3444e88785aaada72e120e458829d
dongwookang <dwk9181@yahoo.com>
2021-10-08 16:55:48 +0900

    d

--- landmarkApp/urls.py
+++ landmarkApp/urls.py
@@ -6,5 +6,6 @@ app_name = 'landmarkApp'
 
 urlpatterns = [
     path('hotel/', views.hotel_recommendation, name='hotel'),
-    path('restaurant/', views.restaurant_recommendation, name='restaurant')
+    path('restaurant/', views.restaurant_recommendation, name='restaurant'),
+    path('info/', views.info, name='info')
 ]
--- landmarkApp/views.py
+++ landmarkApp/views.py
@@ -1,3 +1,6 @@
+import functools
+import operator
+
 import simplejson
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
@@ -7,6 +10,10 @@ import pymysql
 from django.shortcuts import render, redirect
 from .models import Landmarks, Hotels, Restaurants
 from haversine import haversine
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import urllib.request
+import urllib.parse
 
 import numpy as np
 
@@ -115,7 +122,7 @@ def hotel_recommendation(request):
                                               distance_hot_final[i][3], distance_hot_final[i][4])
 
 
-    return JsonResponse(new_dict, safe=False)
+    return JsonResponse(new_dict, safe=True)
 
 
 def restaurant_recommendation(request):
@@ -171,4 +178,106 @@ def restaurant_recommendation(request):
                                               distance_res_final[i][3], distance_res_final[i][4], distance_res_final[i][5])
 
     return HttpResponse(simplejson.dumps(new_dict))
+
+def info(request):
+
+    # search='광화문집'
+    #
+    # conn = pymysql.connect(host='localhost',
+    #                              user='dwk9181',
+    #                              password='',
+    #                              database='final_db',
+    #                              )
+    #
+    # cur = conn.cursor()
+    # sql = "select address from landmarkapp_restaurants where name = %s"
+    # cur.execute(sql, search)
+    # output = cur.fetchall()
+    #
+    #
+    # for i in output:
+    #     output = i
+    #
+    #     for i in output:
+    #         output = i
+    #
+    #
+    # conn.close
+    #
+    # search1 = search + ' ' + output
+    #
+    # encode = urllib.parse.quote_plus(search1)
+    #
+    # response = urlopen('https://map.naver.com/v5/search/' + encode)
+    # soup = BeautifulSoup(response, 'html.parser')
+    #
+    # anc = soup.select('span.place_blind > em')
+    #
+    #
+    # context ={
+    #
+    #     'anc': anc,
+    #     'soup': soup,
+    #
+    # }
+    from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
+    import time
+    from bs4 import BeautifulSoup
+
+    driver = webdriver.Chrome()
+    driver.get("https://map.naver.com/v5/search")
+
+    # 팝업 창 제거
+    driver.find_element_by_css_selector("button#intro_popup_close").click()
+
+    # 검색창에 검색어 입력하기
+    search_box = driver.find_element_by_css_selector("div.input_box>input.input_search")
+    search_box.send_keys("마라탕")
+
+    time.sleep(3)
+
+    # 검색버튼 누르기
+    search_box.send_keys(Keys.ENTER)
+
+    # 크롤링
+    for p in range(20):
+        # 5초 delay
+        time.sleep(2)
+
+        js_script = "document.querySelector(\"body > app > layout > div > div.container > div.router-output > " \
+                    "shrinkable-layout > search-layout > search-list > search-list-contents > perfect-scrollbar\").innerHTML"
+        raw = driver.execute_script("return " + js_script)
+
+        html = BeautifulSoup(raw, "html.parser")
+
+        contents = html.select("div > div.ps-content > div > div > div .item_search")
+        for s in contents:
+            search_box_html = s.select_one(".search_box")
+
+            name = search_box_html.select_one(".title_box .search_title .search_title_text").text
+            print("식당명: " + name)
+            try:
+                phone = search_box_html.select_one(".search_text_box .phone").text
+            except:
+                phone = "NULL"
+            print("전화번호: " + phone)
+            address = search_box_html.select_one(".ng-star-inserted .address").text
+            print("주소: " + address)
+
+            print("--" * 30)
+        # 다음 페이지로 이동
+        try:
+            next_btn = driver.find_element_by_css_selector("button.btn_next")
+            next_btn.click()
+        except:
+            print("데이터 수집 완료")
+            break
+
+    # 크롭 웹페이지를 닫음
+    driver.close()
+
+
+    return render(request, 'restaurant/info.html')
+
 
